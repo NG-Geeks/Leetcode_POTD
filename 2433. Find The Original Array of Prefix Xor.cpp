3c32a4e11293631bcccab8962c89b0e743e6eea0
Nilesh <nilesh.20gcebit040@galgotiacollege.edu>
2023-10-31 08:15:42 +0530

    31 OCT Done

---
+++ 2433. Find The Original Array of Prefix Xor.cpp
@@ -0,0 +1,21 @@
+Ques::  2433. Find The Original Array of Prefix Xor
+
+ You are given an integer array pref of size n. Find and return the array arr of size n that satisfies:
+
+pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+Note that ^ denotes the bitwise-xor operation.
+It can be proven that the answer is unique.
+
+Solution::
+class Solution {
+public:
+      vector<int> findArray(vector<int>& pref) {
+        int n = pref.size();
+        
+        for (int i = n - 1; i > 0; i--) {
+            pref[i] = pref[i] ^ pref[i - 1];
+        }
+        
+        return pref;
+    }
+};
