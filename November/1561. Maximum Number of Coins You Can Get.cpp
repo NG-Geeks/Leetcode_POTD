67c9cbe56c40aa2e15135e963f550681c9fbc7df
Nilesh <nilesh.20gcebit040@galgotiacollege.edu>
2023-11-24 09:56:24 +0530

    24 Nov 2023

--- November/1561. Maximum Number of Coins You Can Get.cpp
+++ November/1561. Maximum Number of Coins You Can Get.cpp
@@ -1,17 +1,15 @@
-1630. Arithmetic Subarrays
-A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
 
-For example, these are arithmetic sequences:
+1561. Maximum Number of Coins You Can Get
+There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
 
-1, 3, 5, 7, 9
-7, 7, 7, 7
-3, -1, -5, -9
-The following sequence is not arithmetic:
+In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+Of your choice, Alice will pick the pile with the maximum number of coins.
+You will pick the next pile with the maximum number of coins.
+Your friend Bob will pick the last pile.
+Repeat until there are no more piles of coins.
+Given an array of integers piles where piles[i] is the number of coins in the ith pile.
 
-1, 1, 2, 5, 7
-You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays are 0-indexed.
-
-Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false otherwise.
+Return the maximum number of coins that you can have.
 #pragma GCC optimize("03")
 class Solution {
 public:
