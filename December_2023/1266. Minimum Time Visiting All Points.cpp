152a620b6a5c7269a147372c3d22ffea596e4c02
Nilesh <kaptaan10c@gmail.com>
2023-12-03 07:37:22 +0530

    3 Nov 2023w

---
+++ December_2023/1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,37 @@
+
+1266. Minimum Time Visiting All Points
+
+On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+
+You can move according to these rules:
+
+In 1 second, you can either:
+move vertically by one unit,
+move horizontally by one unit, or
+move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.
+
+
+Solution::
+
+#pragma gcc optimize("03")
+auto init =[](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int cnt = 0;
+        int x  = points[0][0] , y = points[0][1];
+        for(int i=1;i<points.size();i++){
+                cnt += (max(abs(points[i][0] - x)  ,abs( points[i][1] - y)));
+                x = points[i][0] ;
+                y = points[i][1];
+        }
+        return cnt;
+    }
+};
