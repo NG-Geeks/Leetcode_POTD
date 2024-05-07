6deb8104dff45329b95d5f26c94436c514fb6fa2
Nilesh <kaptaan10c@gmail.com>
2024-05-07 08:41:50 +0530

    2487. Remove Nodes From Linked List

---
+++ May_2024/2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,42 @@
+Question :: 
+2487. Remove Nodes From Linked List
+
+You are given the head of a linked list.
+
+Remove every node which has a node with a greater value anywhere to the right side of it.
+
+Return the head of the modified linked list.
+
+
+
+Complexity:
+Time complexity:
+    O(N)
+Space complexity:
+    O(N)
+Code :
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        ListNode* cur = head;
+        stack<ListNode*> stack;
+        
+        while (cur != nullptr) {
+            while (!stack.empty() && stack.top()->val < cur->val) {
+                stack.pop();
+            }
+            stack.push(cur);
+            cur = cur->next;
+        }
+        
+        ListNode* nxt = nullptr;
+        while (!stack.empty()) {
+            cur = stack.top();
+            stack.pop();
+            cur->next = nxt;
+            nxt = cur;
+        }
+        
+        return cur;
+    }
+};
